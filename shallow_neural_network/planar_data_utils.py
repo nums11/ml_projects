29def90ca8633e37cf5ae88d920aeda1f0920a4e
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2021-12-13 16:34:53 -0800

    Begin adding TF

--- shallow_neural_network/planar_data_utils.py
+++ shallow_neural_network/planar_data_utils.py
@@ -21,6 +21,23 @@ def plot_decision_boundary(model, X, y):
     plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
     plt.show()
     
+def plot_decision_boundary_custom(model, X, y):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model(np.c_[xx.ravel(), yy.ravel()].T)
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
+    plt.show()
+    
 
 def sigmoid(x):
     """
--- shallow_neural_network/shallow_neural_network.py
+++ shallow_neural_network/shallow_neural_network.py
@@ -38,12 +38,23 @@ to calculate dZ).
 for these calculations are the same across all layers except for dZ on the last layer
 - Using the * operator or np.multiply does element-wise multiplication. Using np.matmul
 does matrix multiplication.
+
+TF Learnings
+- When creating an input layer the first parameter is actually the num of features
+(e.g. shape=(2,) for 2 features). This is a little weird since the rest of stuff has
+the first param as the number of rows in the data
+- When counting the num of trainable paramters don't forget that it counts the biases.
+- You can get the weights and biases for a particular layer i with model.layers[i].get_weights()
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
-from planar_data_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+from planar_data_utils import plot_decision_boundary, plot_decision_boundary_custom, sigmoid, load_planar_dataset, load_extra_datasets
 from sklearn.linear_model import LogisticRegression
+from tqdm import tqdm
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.python.keras.layers import Input, Dense
 
 class Layer(object):
 	def __init__(self, num_units, num_units_in_prev_layer):
@@ -52,8 +63,6 @@ class Layer(object):
 		self.B = np.zeros((self.num_units, 1))
 		self.Z = np.zeros((self.num_units, 1))
 		self.A = np.zeros((self.num_units, 1))
-
-		print("Initialized Layer")
 
 class ShallowNeuralNetwork(object):
 	def __init__(self, num_hidden_units):
@@ -65,39 +74,63 @@ class ShallowNeuralNetwork(object):
 		self.X = X
 		self.Y = Y
 		self.m = self.X.shape[1]
-		# print(self.Y)
-		# print(self.X.shape[0])
+		self.learning_rate = alpha
 		self.hidden_layer = Layer(self.num_hidden_units, self.X.shape[0])
 		self.output_layer = Layer(1, self.num_hidden_units)
-		self.forwardProp()
-		self.backProp()
+		self.costs = []
+		for i in tqdm(range(num_iterations)):
+			self.forwardProp()
+			self.backProp()
+		# self.plotCostOverTime()
 
 	def forwardProp(self):
 		self.hidden_layer.Z = \
 			np.dot(self.hidden_layer.W, self.X) + self.hidden_layer.B
-		# print("Z for hidden layer", self.hidden_layer.Z)
 		self.hidden_layer.A = sigmoid(self.hidden_layer.Z)
-		# print("A for hidden layer", self.hidden_layer.A)
 		self.output_layer.Z = \
 			np.dot(self.output_layer.W, self.hidden_layer.A) + self.output_layer.B
-		# print("Z for output_layer", self.output_layer.Z)
 		self.output_layer.A = np.tanh(self.output_layer.Z)
-		# print("A for output layer", self.output_layer.A)
+		self.costs.append(self.getCost(self.output_layer.A, self.Y))
 
 	def backProp(self):
 		dZ_output_layer = self.output_layer.A - self.Y
-		# print(self.output_layer.Z.shape)
-		# print(dZ_output_layer.shape)
 		dW_output_layer = (1/self.m) * np.dot(dZ_output_layer, self.hidden_layer.A.T)
-		# print(self.output_layer.W.shape)
-		# print(dW_output_layer.shape)
 		db_output_layer = (1/self.m) * np.sum(dZ_output_layer, axis=1, keepdims=True)
-		# print(self.output_layer.B.shape)
-		# print(db_output_layer.shape)
 		dZ_hidden_layer = \
 			np.dot(self.output_layer.W.T, dZ_output_layer) * (self.hidden_layer.A * (1 - self.hidden_layer.A))
-		print(self.hidden_layer.Z.shape)
-		print(dZ_hidden_layer.shape)
+		dW_hidden_layer = (1/self.m) * np.dot(dZ_hidden_layer, self.X.T)
+		db_hidden_layer = (1/self.m) * np.sum(dZ_hidden_layer, axis=1, keepdims=True)
+		self.hidden_layer.W = self.hidden_layer.W - self.learning_rate * dW_hidden_layer
+		self.hidden_layer.B = self.hidden_layer.B - self.learning_rate * db_hidden_layer
+		self.output_layer.W = self.output_layer.W - self.learning_rate * dW_output_layer
+		self.output_layer.B = self.output_layer.B - self.learning_rate * db_output_layer
+
+	def getCost(self, predictions, Y):
+		cost = (-1 / self.m) * np.sum(Y * np.log(predictions) + (1 - Y) * np.log(1 - predictions))
+		return cost
+
+	def plotCostOverTime(self):
+		plt.plot(self.costs)
+		plt.show()
+
+	def predict(self, X):
+		hidden_layer_Z = np.dot(self.hidden_layer.W, X) + self.hidden_layer.B
+		hidden_layer_A = sigmoid(hidden_layer_Z)
+		output_layer_Z = np.dot(self.output_layer.W, hidden_layer_A) + self.output_layer.B
+		output_layer_A = np.tanh(output_layer_Z)
+		return output_layer_A
+
+	def test(self, X, Y):
+		predictions = self.predict(X).flatten()
+		print("Regular predictions", predictions)
+		discrete_predictions = [1 if x >= 0.5 else 0 for x in predictions]
+		# print("discrete_predictions",discrete_predictions)
+		return getAccuracy(discrete_predictions, Y.flatten())
+
+def getAccuracy(y_predictions, y_test):
+	num_correct = np.array([True if i == j else False for i,j in zip(y_predictions, y_test)]).sum()
+	return num_correct / len(y_test)
+
 
 def main():
 	X, Y = load_planar_dataset()
@@ -114,9 +147,34 @@ def main():
 	# log_reg_classifier = LogisticRegression(random_state=0).fit(X, Y.flatten())
 	# plot_decision_boundary(lambda x: log_reg_classifier.predict(x), X.T, Y.T)
 
-	nn = ShallowNeuralNetwork(4)
-	nn.fit(X.T, Y.T, 0.1, 100)
+	# shallow_nn = ShallowNeuralNetwork(4)
+	# shallow_nn.fit(X.T, Y.T, 0.1, 10)
+	# # print(X.T.shape)
+	# first_sample = np.array([
+	# 	[X.T[0][0]],
+	# 	[X.T[1][0]]
+	# 	])
+	# # print("First sample", first_sample)
+	# accuracy = shallow_nn.test(X.T,Y.T)
+	# print("accuracy", accuracy)
+	# shallow_nn_predictions = shallow_nn.predict(X.T)
+	# shallow_nn_cost = shallow_nn.getCost(shallow_nn_predictions, Y.T)
+	# print(shallow_nn_cost)
+	# print(Y.T.shape)
 
+	# plot_decision_boundary_custom(lambda x: shallow_nn.predict(x), X.T, Y.T)
 
+	# Might need to change from tanh to sigmoid
+	# TF appears to implement where each row represents a sample and each column represents a unit
+	# Implement TF then go back and change implementation to use softmax (check that all the outputs are
+	# actually 0 to 1)
+
+	# Creating the model with 12 trainable parameters
+	model = Sequential()
+	# First parameter of shape is the number of features
+	model.add(Input(shape=(2,)))
+	model.add(Dense(4, activation='sigmoid'))
+	model.add(Dense(1, activation='tanh'))
+	model.summary()
 
 main()
