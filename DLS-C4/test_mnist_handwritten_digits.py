00c7f29b3051c0f02ebf92fe714a77554282736e
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2021-12-28 19:29:30 -0500

    Train simple architecture

--- DLS-C4/test_mnist_handwritten_digits.py
+++ DLS-C4/test_mnist_handwritten_digits.py
@@ -5,6 +5,11 @@ Num classes = 10 for each of the handwritten digits (0 - 9)
 Training set: 60,0000 samples
 Test set: 10,000 samples
 
+Results:
+- Surprisingly, with such a simple conv architecture that just has 1 hidden layer that
+does a convolution with 1 3x3 filter is able to reach around 86% accuracy after just 6
+iterations. I'm also surprised how slow it trains. Maybe it's because there's so many samples?
+
 Learnings:
 - The main difference between CNNs and FFNNs is that the calculation for Z is not W * X + b but
 rather the convolution of W on X then adding b. And instead of having multiple units and a bias
@@ -19,31 +24,39 @@ only count layers as layers with weights so they don't count the pooling layer a
 	based on only a small region from the input. It's almost like if you had 2 layers but instead of being
 	fully connected, they were only partially connected so a unit in layer 2 only had weights from some of
 	the units in layer 1. This also decreases the number of parametrs.
+- When passing images to TF, it expects a 4-d array (you have to specify the number of channels) so you
+may have to reshape data
 
 ToDo:
 - Test it out against KNN?
 - Try on other image datasets not just MNIST?
 - Create my own dataset and try it?
 - Try it on letters and Japanese characters
+- Fashion MNIST?
+- Adding batch GD?
 """
 
 import numpy as np
-from mnist import MNIST
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.python.keras.layers import Input, Dense, Conv2D, Flatten
+from tensorflow.keras.datasets import mnist
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
-mndata = MNIST('DLS-C4/data')
-X_train, Y_train = mndata.load_training()
-X_test, Y_test = mndata.load_testing()
+X_train = X_train.reshape(-1, 28, 28, 1)
+X_train = tf.cast(X_train, tf.float64)
+X_test = X_test.reshape(-1, 28, 28, 1)
+X_test = tf.cast(X_test, tf.float64)
 num_classes = 10
 
-# Is it possible to convolve flattened images or should I unflaten?
+def displayDataPoint(index):
+	plt.imshow(X_train[index], cmap=plt.get_cmap('gray'))
+	plt.show()
 
 def testTF():
 	model = Sequential()
-	model.add(Input(shape=(28,28,1)))
+	# model.add(Input(shape=(1,28,28)))
 	model.add(Conv2D(1, 3, activation='relu'))
 	model.add(Flatten())
 	model.add(Dense(num_classes, activation='softmax'))
@@ -52,22 +65,10 @@ def testTF():
 		loss=tf.keras.losses.SparseCategoricalCrossentropy(),
 		metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]
 	)
-	model.summary()
-
-
-	# model.add(Input(shape=(2,)))
-	# model.add(Dense(4, activation='relu'))
-	# model.add(Dense(6, activation='softmax'))
-	# model.compile(
-	# 	optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
-	# 	loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-	# 	metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]
-	# )
 	# model.summary()
-	# training_history = model.fit(X, Y, epochs=1000)
-	# plt.plot(training_history.history["loss"])
-	# plt.show()
-	# print(model.evaluate(X, Y, return_dict=True)['sparse_categorical_accuracy'])
-
+	training_history = model.fit(X_train, Y_train, epochs=10)
+	plt.plot(training_history.history["loss"])
+	plt.show()
+	print(model.evaluate(X_test, Y_test, return_dict=True)['sparse_categorical_accuracy'])
 
 testTF()
